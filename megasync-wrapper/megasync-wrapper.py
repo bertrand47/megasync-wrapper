dfa66f708e6bc1833b0dfe88e730399b1e01bd8a
Stefan Ric <stfric369@gmail.com>
2014-12-23 14:30:17 +0100

    Gotta start checking for errors before pushing :/

--- megasync-wrapper/megasync-wrapper.py
+++ megasync-wrapper/megasync-wrapper.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         subprocess.call([mega_bin]);
         exit(1)
         
-    pid = subprocess.Popen([mega_bin]).pid
+    pid = subprocess.Popen([mega_bin], env=mega_env).pid
     substr = "sni-qt_megasync_{0}".format(pid)
 
     finished = False
@@ -43,5 +43,7 @@ if __name__ == '__main__':
     
     mega_icon = icons_folder + 'megasync_{0}_5e6bbab03e062640f0a3c6c540f119a7.png'.format(pid)
     shutil.copy(subst_icons_folder + 'megasync.png', mega_icon)
+    os.chmod(mega_icon, stat.S_IRUSR)
     mega_icon1 = icons_folder + 'megasync_{0}_a8fb04239cb3d37ca409bd8a55eaa7d8.png'.format(pid)
     shutil.copy(subst_icons_folder + 'megasync1.png', mega_icon1)
+    os.chmod(mega_icon1, stat.S_IRUSR)
